0f59b2147e26852b7bda162e660aac84e63a1698
Bora <bora@Bora-MacBook-Air.local>
2023-03-11 19:33:23 +0300

    created BookService

---
+++ src/main/java/com/bookhive/springbootlibrary/service/BookService.java
@@ -0,0 +1,49 @@
+package com.bookhive.springbootlibrary.service;
+
+import com.bookhive.springbootlibrary.dao.BookRepository;
+import com.bookhive.springbootlibrary.dao.CheckoutRepository;
+import com.bookhive.springbootlibrary.entity.Book;
+import com.bookhive.springbootlibrary.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    private final CheckoutRepository checkoutRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
+            throw new Exception("Book does not exist or already checkout by the user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        bookRepository.save(book.get());
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDateTime.now().toString(),
+                LocalDateTime.now().plusDays(7).toString(),
+                book.get().getId()
+        );
+
+        checkoutRepository.save(checkout);
+
+        return book.get();
+    }
+}
