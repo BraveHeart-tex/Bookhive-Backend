3b17f7edb84e0be2c1476e6d48505b98d94234de
Bora <bora@Bora-MacBook-Air.local>
2023-03-13 23:49:54 +0300

    created requestmodels with ReviewRequest

---
+++ src/main/java/com/bookhive/springbootlibrary/requestmodels/ReviewRequest.java
@@ -0,0 +1,12 @@
+package com.bookhive.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+    private double rating;
+    private long bookId;
+    private Optional<String> reviewDescription;
+}
